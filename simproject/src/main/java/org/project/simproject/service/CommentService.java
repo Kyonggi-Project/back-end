155b2e46a2fc20d6e49bb39322874671ed7f0a03
seablood <dudwnszero99@gmail.com>
2024-01-18 18:20:32 +0900

    A: 댓글 수정용 Request, CommentService 생성 및 구현

---
+++ simproject/src/main/java/org/project/simproject/service/CommentService.java
@@ -0,0 +1,42 @@
+package org.project.simproject.service;
+
+import jakarta.transaction.Transactional;
+import org.project.simproject.domain.Comment;
+import org.project.simproject.dto.AddCommentRequest;
+import org.project.simproject.dto.ModifyCommentRequest;
+import org.project.simproject.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public Comment save(AddCommentRequest request, Long articleId, String nickname){
+        return commentRepository.save(request.toEntity(articleId, nickname));
+    }
+
+    public List<Comment> findToArticleId(Long articleId){
+        List<Comment> list = commentRepository.findByArticleId(articleId);
+
+        return list;
+    }
+
+    public void delete(Long id){
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Comment Not Found"));
+        commentRepository.delete(comment);
+    }
+
+    @Transactional
+    public Comment modify(ModifyCommentRequest request, Long id){
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Comment Not Found"));
+        comment.modify(request);
+
+        return  comment;
+    }
+}
