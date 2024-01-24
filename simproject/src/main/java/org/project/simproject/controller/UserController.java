7835111e00bbb6119774d7ce64e18e8fe220e5f7
Chanproheal <rlacksrb0529@gmail.com>
2024-01-24 09:22:23 +0900

    F : mapping Sign -> sign 변경 F : 유저 정보 수정 PostMapping -> PutMapping 변경 A : signup DTO 추가

--- simproject/src/main/java/org/project/simproject/controller/UserController.java
+++ simproject/src/main/java/org/project/simproject/controller/UserController.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.project.simproject.domain.User;
 import org.project.simproject.dto.AddUserRequest;
 import org.project.simproject.dto.ModifyRequest;
+import org.project.simproject.dto.UserResponse;
 import org.project.simproject.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,17 +18,16 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "유저 시스템", description = "유저 관련 기능")
 public class UserController {
 
-    @Autowired
     private final UserService userService;
 
     //유저 추가
     @Operation(summary = "유저 추가", description = "유저 서비스에서 유저를 추가")
-    @PostMapping("/Signup")
-    public ResponseEntity<User> Signup(@RequestBody AddUserRequest addUserRequest) {
+    @PostMapping("/signup")
+    public ResponseEntity<UserResponse> signup(@RequestBody AddUserRequest addUserRequest) {
         try {
             User createuser = userService.createUser(addUserRequest);
-            createuser.setArticlesCount(0);
-            return new ResponseEntity<>(createuser, HttpStatus.CREATED);
+            UserResponse userResponse = new UserResponse(createuser);
+            return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
         }catch (Exception e) {
             return ResponseEntity.internalServerError().body(null);
         }
@@ -43,20 +42,21 @@ public class UserController {
     }
     //현재 유저 수정
     @Operation(summary = "유저 정보 수정", description = "유저 서비스에서 현재 유저의 정보를 수정")
-    @PostMapping("/modify")
+    @PutMapping("/modify")
     public ResponseEntity<User> userModify(@RequestParam String email, @RequestBody ModifyRequest modifyRequest) {
         User modifyuser = userService.updateUser(email,modifyRequest);
         return new ResponseEntity<>(modifyuser,HttpStatus.OK);
     }
+
     //유저 삭제
     @Operation(summary = "유저 삭제", description = "유저 서비스에서 해당 유저를 삭제")
-    @DeleteMapping("/delete/{email}")
-    public ResponseEntity<String> delete(@RequestParam String email) {
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> delete(@RequestParam Long id) {
         try {
-            userService.delete(email);
+            userService.delete(id);
             return ResponseEntity.ok("The user was deleted.");
         } catch (Exception e) {
-            return ResponseEntity.internalServerError().body("You can't delete a user." + e.getMessage());
+            return ResponseEntity.internalServerError().body("You can't delete a user.");
         }
     }
 }
