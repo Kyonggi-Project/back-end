ebbc2cd094526207df690764b1020ece92063c2e
seablood <dudwnszero99@gmail.com>
2024-01-25 18:35:22 +0900

    A: FollowController 추가

---
+++ simproject/src/main/java/org/project/simproject/controller/FollowController.java
@@ -0,0 +1,41 @@
+package org.project.simproject.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.project.simproject.dto.UserResponse;
+import org.project.simproject.service.FollowService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/follow")
+@Tag(name = "팔로우 시스템", description = "팔로우 관련 기능")
+public class FollowController {
+    private final FollowService followService;
+
+    @Operation(summary = "팔로우 추가 및 삭제", description = "팔로우 추가/삭제는 Service단에서 실행")
+    @PostMapping("/following/{followeeEmail}")
+    public ResponseEntity<Void> followToggle(@RequestParam Long id, @PathVariable String followeeEmail){
+        followService.toggle(id, followeeEmail);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @Operation(summary = "특정 유저의 팔로워 목록 보기", description = "임시 controller")
+    @GetMapping("/follower/{email}")
+    public ResponseEntity<List<UserResponse>> getFollower(@PathVariable String email){
+        List<UserResponse> list = followService.findListOfFollower(email);
+        return ResponseEntity.status(HttpStatus.OK).body(list);
+    }
+
+    @Operation(summary = "특정 유저의 팔로잉 목록 보기", description = "임시 controller")
+    @GetMapping("/following/{email}")
+    public ResponseEntity<List<UserResponse>> getFollowing(@PathVariable String email){
+        List<UserResponse> list = followService.findListOfFollowee(email);
+        return ResponseEntity.status(HttpStatus.OK).body(list);
+    }
+}
