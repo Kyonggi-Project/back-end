3bac4babc5456024f8e22f79f872b052d74dcce8
seablood <dudwnszero99@gmail.com>
2024-01-31 18:56:28 +0900

    A: JUnit 기반 테스트 코드 추가

---
+++ simproject/src/test/java/org/project/simproject/controller/TokenApiControllerTest.java
@@ -0,0 +1,88 @@
+package org.project.simproject.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.project.simproject.config.jwt.JwtFactory;
+import org.project.simproject.config.jwt.Properties;
+import org.project.simproject.domain.RefreshToken;
+import org.project.simproject.domain.User;
+import org.project.simproject.dto.RefreshTokenRequest;
+import org.project.simproject.repository.RefreshTokenRepository;
+import org.project.simproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TokenApiControllerTest {
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private Properties properties;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        userRepository.deleteAll();
+    }
+
+    @DisplayName("리프레시 토큰을 이용한 액세스 토큰 재발급")
+    @Test
+    public void test() throws Exception{
+            final String url = "/api/token/createToken";
+
+            User user = User.builder()
+                    .email("test@gmail.com")
+                    .password("test")
+                    .nickname("test")
+                    .build();
+            userRepository.save(user);
+
+            String refreshToken = JwtFactory.builder()
+                    .claims(Map.of("id", user.getId()))
+                    .build()
+                    .createToken(properties);
+            refreshTokenRepository.save(new RefreshToken(user, refreshToken));
+
+            final RefreshTokenRequest request = new RefreshTokenRequest(refreshToken);
+
+            final String requestBody = objectMapper.writeValueAsString(request);
+
+            final ResultActions actions = mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody));
+
+            actions
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.accessToken").isNotEmpty());
+
+    }
+
+
+}
---
+++ simproject/src/test/java/org/project/simproject/controller/TokenProviderTest.java
@@ -0,0 +1,100 @@
+package org.project.simproject.controller;
+
+import io.jsonwebtoken.Jwts;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.project.simproject.config.jwt.JwtFactory;
+import org.project.simproject.config.jwt.JwtTokenProvider;
+import org.project.simproject.config.jwt.Properties;
+import org.project.simproject.domain.User;
+import org.project.simproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class TokenProviderTest {
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private Properties jwtProperties;
+
+    @DisplayName("토큰 생성 테스트")
+    @Test
+    public void test1(){
+        User user = userRepository.save(User.builder()
+                .email("user@gmail.com")
+                .password("test")
+                .build());
+
+        String token = tokenProvider.createToken(user, Duration.ofDays(14));
+
+        Long id = Jwts.parser()
+                .setSigningKey(jwtProperties.getSecret())
+                .parseClaimsJws(token)
+                .getBody()
+                .get("id", Long.class);
+        assertThat(id).isEqualTo(user.getId());
+    }
+
+    @DisplayName("토큰 만료 테스트")
+    @Test
+    public void test2(){
+        JwtFactory jwtFactory = JwtFactory.builder()
+                .expiration(new Date(new Date().getTime() - Duration.ofDays(7).toMillis()))
+                .build();
+
+        String token = jwtFactory.createToken(jwtProperties);
+
+        boolean valid = tokenProvider.validateToken(token);
+
+        assertThat(valid).isFalse();
+    }
+
+    @DisplayName("토큰 유효 테스트")
+    @Test
+    public void test3(){
+        String token = JwtFactory.withDefaultValues().createToken(jwtProperties);
+        boolean valid = tokenProvider.validateToken(token);
+        assertThat(valid).isTrue();
+    }
+
+    @DisplayName("토큰을 통해 인증 객체 생성")
+    @Test
+    public void test4(){
+        String subject = "user@gmail.com";
+        JwtFactory factory = JwtFactory.builder()
+                .subject(subject)
+                .build();
+        String token = factory.createToken(jwtProperties);
+
+        Authentication authentication = tokenProvider.getAuthentication(token);
+
+        assertThat(((UserDetails)authentication.getPrincipal()).getUsername()).isEqualTo(subject);
+    }
+
+    @DisplayName("토큰을 통해 유저 ID 가져오기")
+    @Test
+    public void test5(){
+        Long id = 1L;
+        String token = JwtFactory.builder()
+                .claims(Map.of("id", id))
+                .build()
+                .createToken(jwtProperties);
+
+        Long claim = tokenProvider.getId(token);
+
+        assertThat(claim).isEqualTo(id);
+    }
+}
