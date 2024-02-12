6b02f464851614a3df531194c337224637c7bcd8
seablood <dudwnszero99@gmail.com>
2024-02-12 16:41:32 +0900

    F: 회원가입 Test 구현

--- simproject/src/main/java/org/project/simproject/controller/TestViewController.java
+++ simproject/src/main/java/org/project/simproject/controller/TestViewController.java
@@ -1,12 +1,35 @@
 package org.project.simproject.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.project.simproject.dto.AddUserRequest;
+import org.project.simproject.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class TestViewController {
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public String save(AddUserRequest addUserRequest){
+        userService.createUser(addUserRequest);
+        return "redirect:/login";
+    }
+
     @GetMapping("/oauthLogin")
     public String oauthLogin(){
         return "oauthLogin";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signUp(){
+        return "signup";
     }
 }
