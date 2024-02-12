67c9ca05d25bd44257907b3731fb38e860d4ef58
seablood <dudwnszero99@gmail.com>
2024-02-12 16:54:55 +0900

    A: Form Login 시 사용할 UserDetailService 추가

---
+++ simproject/src/main/java/org/project/simproject/service/UserDetailService.java
@@ -0,0 +1,19 @@
+package org.project.simproject.service;
+
+import lombok.RequiredArgsConstructor;
+import org.project.simproject.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+    }
+}
