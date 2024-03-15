13bc346fae79f4c672999c83f597291c8f93366e
seablood <dudwnszero99@gmail.com>
2024-03-15 09:35:48 +0900

    F: 개발 편리를 위한 리소스 설정 수정

--- simproject/src/main/java/org/project/simproject/config/CorsConfig.java
+++ simproject/src/main/java/org/project/simproject/config/CorsConfig.java
@@ -3,6 +3,7 @@ package org.project.simproject.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +16,13 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("classpath:/static/img/");
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
     }
 }
