bd2dc8ffcf04e0b405b61f428159671ea55eac9f
seablood <dudwnszero99@gmail.com>
2024-03-19 17:14:46 +0900

    F: 크롤링 테스트 코드 추가(스크롤하여 크롤링)

--- simproject/src/main/java/org/project/simproject/service/CrawlingService.java
+++ simproject/src/main/java/org/project/simproject/service/CrawlingService.java
@@ -2,7 +2,10 @@ package org.project.simproject.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +13,8 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -18,12 +23,15 @@ import java.util.concurrent.TimeUnit;
 @PropertySource("classpath:application-crawling.properties")
 public class CrawlingService {
     private WebDriver webDriver;
+
+    private JavascriptExecutor jse;
+
     @Value("${driver.chrome.driver_path}")
     private String WEB_DRIVER_PATH;
 
     public void startCrawling() throws IOException, InterruptedException {
 
-        String url = "https://www.netflix.com/kr/browse/genre/839338";
+        String url = "https://m.kinolights.com/discover/explore";
 
         log.info("OTT 크롤링");
 
@@ -40,10 +48,45 @@ public class CrawlingService {
 
         webDriver.get(url);
 
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        // 넷플릭스 버튼 클릭
+        WebElement movieItem = webDriver.findElement(By.xpath("//*[@id=\"contents\"]/section/div[2]/div/div/div/div[2]/button"));
+        jse = (JavascriptExecutor) webDriver;
+        jse.executeScript("arguments[0].click();", movieItem);
+
+        webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
         System.out.println(webDriver.getTitle());
 
-        webDriver.quit();
+        // 10번 스크롤하여 나온 모든 컨텐츠의 링크 저장 및 출력(Test)
+        scrollDownAndCollect(webDriver, jse);
+
+        webDriver.close();
+    }
+
+    // 키노라이츠 사이트 등록 컨텐츠 중 넷플릭스 컨텐츠만 크롤링(Test)
+    public static void scrollDownAndCollect(WebDriver driver, JavascriptExecutor js) throws InterruptedException {
+
+        for (int i = 0; i < 5; i++) { // 5번 스크롤 내리기
+            js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+
+            // 페이지 로딩까지 대기
+            Thread.sleep(1000);
+
+
+            List<WebElement> movieItems = driver.findElements(By.cssSelector("div.MovieItem"));
+
+            List<WebElement> anchorTags = new ArrayList<>();
+            for(WebElement movieItem : movieItems){
+                anchorTags.add(movieItem.findElement(By.tagName("a")));
+            }
+
+            for (WebElement anchorTag : anchorTags) {
+                System.out.println("찾은 <a> 태그: " + anchorTag.getAttribute("href"));
+            }
+            System.out.println();
+            System.out.println();
+        }
     }
 }
