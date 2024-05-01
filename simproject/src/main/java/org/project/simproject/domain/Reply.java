c73ccc423dbec2dfe31dd41a2484f05ae1ba9726
seablood <dudwnszero99@gmail.com>
2024-05-01 21:42:42 +0900

    A: Reply Domain 추가

---
+++ simproject/src/main/java/org/project/simproject/domain/Reply.java
@@ -0,0 +1,42 @@
+package org.project.simproject.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "replies")
+public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ott_review_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private OTTReview ottReviewId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User userId;
+
+    private String content;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public Reply(String content, User userId, OTTReview ottReviewId){
+        this.content = content;
+        this.userId = userId;
+        this.ottReviewId = ottReviewId;
+        this.updatedAt = LocalDateTime.now();
+    }
+}
