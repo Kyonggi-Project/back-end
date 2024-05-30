bc1c96af0d48a85ebede7a781e4ec1479981117d
seablood <dudwnszero99@gmail.com>
2024-05-30 15:12:19 +0900

    F: 채팅방 참가 인원수 업데이트 추가

--- simproject/src/main/java/org/project/simproject/controller/MessageController.java
+++ simproject/src/main/java/org/project/simproject/controller/MessageController.java
@@ -1,8 +1,10 @@
 package org.project.simproject.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.project.simproject.domain.ChatRoom;
 import org.project.simproject.dto.request.AddMessageRequest;
 import org.project.simproject.dto.response.MessageResponse;
+import org.project.simproject.service.ChatRoomService;
 import org.project.simproject.service.MessageService;
 import org.project.simproject.util.ChatMessageStatus;
 import org.springframework.http.HttpStatus;
@@ -23,12 +25,16 @@ public class MessageController {
     private final MessageService messageService;
     private final SimpMessagingTemplate template;   // 입장한 적이 있는 사용자인지 구분하여 전송하기 위한 템플릿
 
+    private final ChatRoomService chatRoomService;
+
     @MessageMapping("/enter/{roomId}")
     public void enterChatRoom(AddMessageRequest addMessageRequest) {
         int enter = messageService.findMessageByRoomIdAndSenderAndStatus(addMessageRequest.getRoomId(),
                 addMessageRequest.getSender(), ChatMessageStatus.ENTER);
         int leave = messageService.findMessageByRoomIdAndSenderAndStatus(addMessageRequest.getRoomId(),
                 addMessageRequest.getSender(), ChatMessageStatus.LEAVE);
+
+        chatRoomService.memberCounting(addMessageRequest.getRoomId(), true);
 
         if((enter - leave) == 0){   // 입장/퇴장 횟수를 이용해 메시지 전송 여부를 판단
             addMessageRequest.setContent(addMessageRequest.getSender() + "님이 입장하였습니다.");
@@ -48,6 +54,8 @@ public class MessageController {
 
     @MessageMapping("/leave/{roomId}")      // 채팅룸 완전히 퇴장
     public void leaveChatRoom(AddMessageRequest addMessageRequest) {
+        chatRoomService.memberCounting(addMessageRequest.getRoomId(), false);
+
         addMessageRequest.setContent(addMessageRequest.getSender() + "님이 퇴장하였습니다.");
         template.convertAndSend("/topic/" + addMessageRequest.getRoomId(),
                 messageService.save(addMessageRequest));
